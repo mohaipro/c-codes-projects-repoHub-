ec323e6d8f8c96afb14150949a8c9a59fd178e63
mohaipro <112693316+mohaipro@users.noreply.github.com>
2025-05-30 02:09:06 +0300

    Create hello.c source file
    just a starting source code for the C language!

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // including the standard input output header file 
+
+int main(void){
+    // greeting message for all C programming lovers :)
+    printf("Hello and Welcome to C Lang!\n");
+}
